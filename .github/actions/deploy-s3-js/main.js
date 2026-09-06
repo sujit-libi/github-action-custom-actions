3327068ce340e519f2319800c556d711874dcf6e
Sujit Kumar Libi <sujitlibi@gmail.com>
2026-09-06 15:49:04 +0545

    Output added

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -22,7 +22,9 @@ function run() {
     const s3Uri = `s3://${bucket}`
     exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`) 
 
-    core.notice("Hello from my custom javascript actin")
+    // core.notice("Hello from my custom javascript actin")
+    const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+    core.setOutput('website-url', websiteUrl) // ::set-output
 }
 
 run()
